c9bf8ccca6a09d8fff3a87fecc2bee1c70e71766
SAY-Hong <bobseahee@gmail.com>
2024-01-05 14:07:36 +0900

    Feat: dismiss 추가

--- FirebaseDemoProjects/EditInformationView.swift
+++ FirebaseDemoProjects/EditInformationView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct EditInformationView: View {
+    @Environment(\.dismiss) private var dismiss
     @ObservedObject var productStore = ProductStore.shared
     @State var name: String = ""
     @State var description: String = ""
@@ -34,6 +35,7 @@ struct EditInformationView: View {
             Button {
                 let editProduct = Product(id: selectedProduct.id, name: selectedProduct.name, description: selectedProduct.description, isOrder: selectedProduct.isOrder)
                 productStore.updateProduct(item: editProduct)
+                dismiss()
             } label: {
                 Text("수정 완료하기")
             }
