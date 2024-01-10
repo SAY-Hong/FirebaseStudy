d046027a219576b695e5740ee9159ba4683c5bb8
SAY-Hong <bobseahee@gmail.com>
2024-01-10 17:32:54 +0900

    코드 의미 메모

--- FirebaseDemoProjects/GoogleLogIn/GoogleLoginManager.swift
+++ FirebaseDemoProjects/GoogleLogIn/GoogleLoginManager.swift
@@ -26,6 +26,7 @@ class GoogleLoginManager: ObservableObject {
         let config = GIDConfiguration(clientID: clientID)
         GIDSignIn.sharedInstance.configuration = config
         
+        // rootViewController
         guard let windowScene = await UIApplication.shared.connectedScenes.first as? UIWindowScene,
               let window = await windowScene.windows.first,
               let rootViewController = await window.rootViewController else {
@@ -34,28 +35,31 @@ class GoogleLoginManager: ObservableObject {
         }
         
         do {
+            // 로그인 진행
             let userAuthentication = try await GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController)
             let user = userAuthentication.user
             guard let idToken = user.idToken else {
                 throw AuthenticationError.tokenError(message: "ID token missing")
             }
+            
+            // Firebase에 구글로 로그인한 계정 추가하기.
+            // 밑의 과정은 파베 쪽에서 uid를 보기 위해 쓴 코드인듯.
+            // 밑의 코드가 없으면 파베에 저장이 안된다.
             let accessToken = user.accessToken
             let credential = GoogleAuthProvider.credential(withIDToken: idToken.tokenString, accessToken: accessToken.tokenString)
             let result = try await Auth.auth().signIn(with: credential)
             let firebaseUser = result.user
+            
             print("User \(firebaseUser.uid) signed in with email \(firebaseUser.email ?? "unknown")")
+            
             return true
         } catch {
             print(error.localizedDescription)
             return false
         }
-    //    return false
     }
+    
     func signOutWithGoogle() {
         
     }
 }
-
-
-
-
---
+++ FirebaseDemoProjects/GoogleLogIn/Users.swift
@@ -0,0 +1,18 @@
+//
+//  Users.swift
+//  FirebaseDemoProjects
+//
+//  Created by 홍세희 on 2024/01/10.
+//
+
+import Foundation
+
+class User {
+    let name: String
+    let email: String
+    
+    init(name: String, email: String) {
+        self.name = name
+        self.email = email
+    }
+}
