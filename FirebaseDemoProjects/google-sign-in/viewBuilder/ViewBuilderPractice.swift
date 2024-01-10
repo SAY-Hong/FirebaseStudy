27e28f25cf5d806eb48c10ee0274ca006e6aa5d0
SAY-Hong <bobseahee@gmail.com>
2024-01-11 01:06:35 +0900

    Feat: extension으로 자동 생성자 생성

--- FirebaseDemoProjects/google-sign-in/viewBuilder/ViewBuilderPractice.swift
+++ FirebaseDemoProjects/google-sign-in/viewBuilder/ViewBuilderPractice.swift
@@ -17,6 +17,8 @@ struct beforeViewBuilder: View {
     }
 }
 
+// MARK: ViewBuilder
+// 뷰를 사용하는 누군가가 자신의 컨텐츠를 전달할 수 있도록 하기 위해 매개변수 제공 -> @ViewBuilder 수정자를 사용하여 클로저 전달.
 struct afterViewBuilder <Content : View> : View {
     let content: Content
     init(@ViewBuilder contentBuilder: () -> Content) {
@@ -31,8 +33,17 @@ struct afterViewBuilder <Content : View> : View {
     }
 }
 
-#Preview {
-    afterViewBuilder {
-        Text("Hello!")
+// afterViewBuilder를 자동으로 채우는 생성자를 작성하기
+// where절의 확장을 사용한다. -> 특정 일반 제약 조건에 대한 확장
+// Preview에서 뷰 안에 내용이 옵셔널일 경우에 화면에 어떻게 나타낼지를 미리 정하는 것.
+extension afterViewBuilder where Content == Color {
+    init() {
+        self.init {
+            Color.black
+        }
     }
+}
+#Preview {
+    afterViewBuilder()
+        .padding()
 }
