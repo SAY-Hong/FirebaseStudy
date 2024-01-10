1b6ab244937d3ba138cc192f9a155d48b1ea8b1b
SAY-Hong <bobseahee@gmail.com>
2024-01-11 00:15:03 +0900

    Move & Add: 폴더링 및 뷰빌더 연습 폴더 생성

---
+++ FirebaseDemoProjects/google-sign-in/viewBuilder/ViewBuilderPractice.swift
@@ -0,0 +1,18 @@
+//
+//  ViewBuilderPractice.swift
+//  FirebaseDemoProjects
+//
+//  Created by 홍세희 on 2024/01/11.
+//
+
+import SwiftUI
+
+struct ViewBuilderPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewBuilderPractice()
+}
