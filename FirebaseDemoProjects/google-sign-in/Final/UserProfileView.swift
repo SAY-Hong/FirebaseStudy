35a25922b9071ed1dcd0b754c9b1e98ac38601ad
SAY-Hong <bobseahee@gmail.com>
2024-01-17 23:05:55 +0900

    Add: UserProfileView 추가하기

---
+++ FirebaseDemoProjects/google-sign-in/Final/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  FirebaseDemoProjects
+//
+//  Created by 홍세희 on 2024/01/17.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text("This is UserProfileView.")
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
